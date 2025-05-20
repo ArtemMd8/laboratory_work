2842a60482d6c69d0a70620e4ed27b46376e2020
ArtemMd8 <jhobin.jo@gmail.com>
2025-05-20 23:34:16 +0300

    laba13

--- lab/lab13.py
+++ lab/lab13.py
@@ -1,29 +1,113 @@
-#1
+def num_tusk(a):
+    if a == 1:
 
-class Restaurant:
-    """Класс для описания ресторана"""
+        #1
+        class Restaurant:
+            def __init__(self, name, type):
+                self.restaurant_name = name
+                self.cuisine_type = type
 
-    def __init__(self, restaurant_name, cuisine_type):
-        """Инициализирует атрибуты названия ресторана и типа кухни"""
-        self.restaurant_name = restaurant_name
-        self.cuisine_type = cuisine_type
+            def describe(self):
+                print(f"Название ресторана: {self.restaurant_name}")
+                print(f"Тип кухни: {self.cuisine_type}")
 
-    def describe_restaurant(self):
-        """Выводит информацию о ресторане"""
-        print(f"Название ресторана: {self.restaurant_name}")
-        print(f"Тип кухни: {self.cuisine_type}")
+            def open(self):
+                print(f"Ресторан '{self.restaurant_name}' открыт!")
 
-    def open_restaurant(self):
-        """Выводит сообщение об открытии ресторана"""
-        print(f"Ресторан '{self.restaurant_name}' открыт!")
+        # Создание экземпляра класса
+        x = Restaurant("китайский", "китайский")
 
-# Создание экземпляра класса
-newRestaurant = Restaurant("Гастрономик", "французская")
+        print(x.restaurant_name)
+        print(x.cuisine_type)
 
-# Вывод атрибутов по отдельности
-print(newRestaurant.restaurant_name)
-print(newRestaurant.cuisine_type)
+        x.describe()
+        x.open()
 
-# Вызов методов
-newRestaurant.describe_restaurant()
-newRestaurant.open_restaurant()
+    if a == 2:
+        #2
+        class Restaurant1:
+            def __init__(self, name, type):
+                self.restaurant_name = name
+                self.cuisine_type = type
+            def describe(self):
+                print(f"Название ресторана: {self.restaurant_name}")
+                print(f"Тип кухни: {self.cuisine_type}")
+            def open(self):
+                print(f"Ресторан '{self.restaurant_name}' закрыт")
+
+        class Restaurant2:
+            def __init__(self, name, type):
+                self.restaurant_name = name
+                self.cuisine_type = type
+            def describe(self):
+                print(f"Название ресторана: {self.restaurant_name}")
+                print(f"Тип кухни: {self.cuisine_type}")
+            def open(self):
+                print(f"Ресторан '{self.restaurant_name}' открыт!")
+
+        class Restaurant3:
+            def __init__(self, name, type):
+                self.restaurant_name = name
+                self.cuisine_type = type
+            def describe(self):
+                print(f"Название ресторана: {self.restaurant_name}")
+                print(f"Тип кухни: {self.cuisine_type}")
+            def open(self):
+                print(f"Ресторан '{self.restaurant_name}' открыт!")
+
+        class Restaurant4:
+            def __init__(self, name, type):
+                self.restaurant_name = name
+                self.cuisine_type = type
+            def describe(self):
+                print(f"Название ресторана: {self.restaurant_name}")
+                print(f"Тип кухни: {self.cuisine_type}")
+            def open(self):
+                print(f"Ресторан '{self.restaurant_name}' закрыт")
+
+
+        # Создание класса
+        x1 = Restaurant1("французский", "французский")
+        x2 = Restaurant2("испанский", "испанский")
+        x3 = Restaurant3("швейцарский", "швейцарский")
+        x4 = Restaurant4("корейский", "корейский")
+
+        x1.describe()
+        x2.describe()
+        x3.describe()
+        x4.describe()
+
+    if a == 3:
+        #3
+        class Restaurant:
+            def __init__(self, name, type, rating):
+                self.restaurant_name = name
+                self.cuisine_type = type
+                self.restaurant_rating = rating
+
+            def describe(self):
+                print(f"Название ресторана: {self.restaurant_name}")
+                print(f"Тип кухни: {self.cuisine_type}")
+                print(f"Рейтинг ресторана : {self.restaurant_rating}")
+
+            def open(self):
+                print(f"Ресторан '{self.restaurant_name}' открыт!")
+
+            def rating_start(self, Nrating):
+                self.restaurant_rating = Nrating
+                print(f"Рейтинг изменён на {self.restaurant_rating} звёзд")
+
+        # Создание класса
+        x = Restaurant("китайский", "китайский", 2.5)
+
+        x.describe()
+        y = int(input('Введите рейтинг ресторана - '))
+        x.rating_start(y)
+
+
+while True:
+    x = int(input('     Введите номер задания: '))
+    if x == 0:
+        break
+    else:
+        num_tusk(x)
